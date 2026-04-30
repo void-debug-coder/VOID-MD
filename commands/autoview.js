9fa7375bca13211e6c07bafda63dc644d8fe5fac
void-debug-coder <mwamwachaezekia@gmail.com>
2026-04-30 21:44:42 +0300

    Update autoview.js

--- commands/autoview.js
+++ commands/autoview.js
@@ -2,11 +2,10 @@ module.exports = {
     name: 'autoview',
     alias: ['av', 'statusview'],
     desc: 'Toggle auto view + react status',
-    category: 'owner',
-    async execute({ reply, config, saveConfig, isOwner }) {
-        if (!isOwner) return reply('Owner only 💀')
-        config.autoview = !config.autoview
+    category: 'public',
+    async execute({ reply, config, saveConfig }) {
+        config.autoview =!config.autoview
         saveConfig()
-        reply(`Autoview: ${config.autoview ? 'ON ✅' : 'OFF ❌'}`)
+        reply(`Autoview: ${config.autoview? 'ON ✅' : 'OFF ❌'}`)
     }
 }
