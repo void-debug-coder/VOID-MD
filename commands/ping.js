7d1a5475bab26bf8e2f78ff427516dd65e9171ac
void-debug-coder <mwamwachaezekia@gmail.com>
2026-04-30 19:44:32 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,12 @@
 module.exports = {
-    name: 'ping', alias: ['speed', 'pong'], desc: 'Check bot response time', category: 'general',
-    async execute({ reply, sock, from, m }) {
+    name: 'ping',
+    alias: ['p', 'speed'],
+    desc: 'Check bot response time',
+    category: 'general',
+    async execute({ reply, uptime }) {
         const start = Date.now()
-        const msg = await reply('*Testing...* 💀')
-        await sock.sendMessage(from, { text: `*Pong!* 🏓\n*Speed:* ${Date.now() - start}ms`, edit: msg.key })
+        await reply('Testing...💀')
+        const end = Date.now()
+        reply(`*Pong!* 🏓\n*Speed:* ${end - start}ms\n*Uptime:* ${uptime()}`)
     }
 }
