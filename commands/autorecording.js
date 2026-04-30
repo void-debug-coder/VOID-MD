bda25188c51ccb6714a1331adc90e89fd126fc98
void-debug-coder <mwamwachaezekia@gmail.com>
2026-04-30 21:36:32 +0300

    Update autorecording.js

--- commands/autorecording.js
+++ commands/autorecording.js
@@ -2,12 +2,11 @@ module.exports = {
     name: 'autorecording',
     alias: ['arc'],
     desc: 'Toggle auto recording presence',
-    category: 'owner',
-    async execute({ reply, config, saveConfig, isOwner }) {
-        if (!isOwner) return reply('Owner only 💀')
-        config.autorecording = !config.autorecording
+    category: 'public',
+    async execute({ reply, config, saveConfig }) {
+        config.autorecording =!config.autorecording
         if (config.autorecording) config.autotyping = false
         saveConfig()
-        reply(`Autorecording: ${config.autorecording ? 'ON ✅' : 'OFF ❌'}`)
+        reply(`Autorecording: ${config.autorecording? 'ON ✅' : 'OFF ❌'}`)
     }
 }
