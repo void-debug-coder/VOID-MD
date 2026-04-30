e9c51cafc70babdfcfcd5d38ce19fac235c9ad29
void-debug-coder <mwamwachaezekia@gmail.com>
2026-04-30 17:31:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+{
+  "PREFIX": ".",
+  "BOT_NAME": "VOID-MD",
+  "OWNER_NUMBER": "254700000000",
+  "autoview": false,
+  "autotyping": false,
+  "autorecording": false,
+  "autonline": false,
+  "autoread": false,
+  "antidelete": false,
+  "antilink": false,
+  "chatbot": false
+}
