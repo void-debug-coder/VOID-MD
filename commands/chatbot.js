52eea5c497aaf7459be36b411a0768d6f42e6468
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-01 03:09:16 +0300

    Update chatbot.js

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -4,9 +4,9 @@ module.exports = {
     desc: 'Toggle auto reply when tagged',
     category: 'owner',
     async execute({ reply, config, saveConfig, isOwner }) {
-        if (!isOwner) return reply('Owner only 💀')
-        config.chatbot = !config.chatbot
+        if (!isOwner) return reply('Only bot number can use this 💀')
+        config.chatbot =!config.chatbot
         saveConfig()
-        reply(`Chatbot: ${config.chatbot ? 'ON ✅' : 'OFF ❌'}`)
+        reply(`Chatbot: ${config.chatbot? 'ON ✅' : 'OFF ❌'}`)
     }
 }
