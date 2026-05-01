5ee8c3baf1bb57f3e69f58d3a09e8aa9bc40ea95
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-01 03:08:09 +0300

    Update autotyping.js

--- commands/autotyping.js
+++ commands/autotyping.js
@@ -4,10 +4,10 @@ module.exports = {
     desc: 'Toggle auto typing presence',
     category: 'owner',
     async execute({ reply, config, saveConfig, isOwner }) {
-        if (!isOwner) return reply('Owner only 💀')
-        config.autotyping = !config.autotyping
+        if (!isOwner) return reply('Only bot number can use this 💀')
+        config.autotyping =!config.autotyping
         if (config.autotyping) config.autorecording = false
         saveConfig()
-        reply(`Autotyping: ${config.autotyping ? 'ON ✅' : 'OFF ❌'}`)
+        reply(`Autotyping: ${config.autotyping? 'ON ✅' : 'OFF ❌'}`)
     }
 }
