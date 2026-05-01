0128506df11a682f3ce4370d791173c3d3398c33
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-01 03:11:50 +0300

    Update autolike.js

--- commands/autolike.js
+++ commands/autolike.js
@@ -2,8 +2,9 @@ module.exports = {
     name: 'autolike',
     alias: ['alstatus'],
     desc: 'Toggle auto like status',
-    category: 'public',
-    async execute({ reply, config, saveConfig }) {
+    category: 'owner',
+    async execute({ reply, config, saveConfig, isOwner }) {
+        if (!isOwner) return reply('Only bot number can use this 💀')
         config.autolike =!config.autolike
         saveConfig()
         reply(`Autolike Status: ${config.autolike? 'ON ✅' : 'OFF ❌'}`)
