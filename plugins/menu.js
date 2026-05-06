bd457009d2e872ef581cde26010eab8501661eaf
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-06 11:15:50 +0300

    Delete plugins directory

--- plugins/menu.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'menu',
-    alias: ['help', 'list'],
-    description: 'Show bot menu',
-    async execute(m, { VoidMD, prefix }) {
-        await m.reply('*VOID-MD Menu* 💀\n\nPrefix: ' + prefix + '\n\nBot is online.')
-    }
-}
