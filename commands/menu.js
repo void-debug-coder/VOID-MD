b344b5d8a6a381d1128c2bc9c487b0c8400bc334
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-06 11:50:22 +0300

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,7 +1,15 @@
 module.exports = {
     name: 'menu',
-    alias: ['help'],
-    async execute(m, { prefix }) {
-        await m.reply(`*VOID-MD Menu* 💀\n\nPrefix: ${prefix}\nCommands loaded successfully.`)
+    alias: ['help', 'list'],
+    async execute(m, { config, commands }) {
+        let text = `*${config.botName} Menu* ${config.themeEmoji}\n\n`
+        text += `*Prefix:* ${config.prefix}\n`
+        text += `*Commands:* ${commands.size}\n\n`
+
+        commands.forEach(cmd => {
+            text += `${config.themeEmoji} ${config.prefix}${cmd.name}\n`
+        })
+
+        await m.reply(text)
     }
 }
