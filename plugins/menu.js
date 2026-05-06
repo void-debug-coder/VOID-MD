96bc821f864459a83fc912ade4f91d38eb7bccb5
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-06 10:47:09 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,13 +1,6 @@
 module.exports = {
     name: 'menu',
-    alias: ['help'],
-    react: '📜',
-    desc: 'Show menu',
-    async execute(m, { VoidMD }) {
-        let text = `*${global.botname} Menu* ${global.themeemoji}\n\n`
-        text += `*Commands:*\n`
-        text += `◦ ${global.prefix}menu - This menu\n\n`
-        text += `_Owner: wa.me/${global.owner}_ ${global.themeemoji}`
-        await VoidMD.sendMessage(m.chat, { text }, { quoted: m })
+    async execute(m) {
+        await m.reply('Menu works')
     }
 }
