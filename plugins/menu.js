ef76b1f06e2a80b4c1b5e5fb73b82f11e586a2df
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-06 11:02:03 +0300

    Refactor menu command to include prefix and status

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: 'menu',
-    async execute(m) {
-        await m.reply('Menu works')
+    alias: ['help', 'list'],
+    description: 'Show bot menu',
+    async execute(m, { VoidMD, prefix }) {
+        await m.reply('*VOID-MD Menu* 💀\n\nPrefix: ' + prefix + '\n\nBot is online.')
     }
 }
