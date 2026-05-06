c337e6c7d32c829be089d59fc68754f3a5d6c3b9
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-06 09:46:57 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,17 +1,13 @@
 module.exports = {
-    ownerNumber: '254112843071', // CHANGE TO YOUR NUMBER - NO + OR SPACES
     botName: 'VOID-MD',
     themeEmoji: '💀',
+    ownerNumber: '254112843071', // Replace with your number
     prefix: '.',
-    version: '3.0.0',
+    version: '1.0.0',
     pairCode: 'VOIDMD',
     storeWriteInterval: 10000,
-    newsletterJids: [
-        '120363230090465542@newsletter',
-        '120363299165217795@newsletter'
-    ],
+    newsletterJids: [],
     channels: {
-        update: 'https://whatsapp.com/channel/0029VbCUCkkAInPkhAPNil1h',
-        support: 'https://chat.whatsapp.com/F0VqKkKJU2DEo2MbIrCqBV'
+        update: 'https://chat.whatsapp.com/F0VqKkKJU2DEo2MbIrCqBV'
     }
 }
