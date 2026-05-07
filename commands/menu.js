be265ebad19a33faf32703728a5a192b012dfb81
void-debug-coder <mwamwachaezekia@gmail.com>
2026-05-07 07:19:17 +0300

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -1,15 +1,11 @@
 module.exports = {
-    name: 'menu',
-    alias: ['help', 'list'],
+    name: 'menu',                    // Command name = .menu
+    alias: ['help', 'list'],         // .help or .list also works
     async execute(m, { config, commands }) {
-        let text = `*${config.botName} Menu* ${config.themeEmoji}\n\n`
-        text += `*Prefix:* ${config.prefix}\n`
-        text += `*Commands:* ${commands.size}\n\n`
-
+        let text = `*${config.botName}* ${config.themeEmoji}\n\n`
         commands.forEach(cmd => {
-            text += `${config.themeEmoji} ${config.prefix}${cmd.name}\n`
+            text += `${config.prefix}${cmd.name}\n`
         })
-
-        await m.reply(text)
+        await m.reply(text)          // m.reply() is built-in now
     }
 }
